7dec801cdb36a444786a3697911371fbc22fb03f
Revaycolizer <109684996+Revaycolizer@users.noreply.github.com>
2023-12-02 11:11:17 +0300

    Add files via upload

---
+++ constants/cookie.ts
@@ -0,0 +1,14 @@
+export const COOKIE_NAME = "OurSchoolJWT";
+export const SEARCHED="searched";
+export const CUSTOMERKEYS = "NAZbeLH26LKbTbbKA0q97QD2HAdKai5r";
+export const SECRETKEYS ="uH6Td5J9YEX6wDJG6rnaQtfdbJM=";
+export const BaseUrl="https://cybqa.pesapal.com/pesapalv3/api/Auth/RequestToken"
+export const Pcallback="https://cybqa.pesapal.com/pesapalv3"
+export const callUrl="https://www.myapplication.com/response-page?OrderTrackingId=b945e4af-80a5-4ec1-8706-e03f8332fb04&OrderMerchantReference=TEST1515111119&OrderNotificationType=CALLBACKURL"
+export const consumer_key="ngW+UEcnDhltUc5fxPfrCD987xMh3Lx8"
+export const consumer_secret="q27RChYs5UkypdcNYKzuUw460Dg="
+export const IPN ="https://www.technologygenius14.com/ipn"
+export const IpnId="e33fb475-4351-4770-a46e-dde082a56c3a"
+export const PIpnId="2cef8b9a-e671-4340-b56e-dde001000ae9"
+export const IpnCall="https://cybqa.pesapal.com/pesapalv3/api/URLSetup/RegisterIPN"
+export const PayCall="https://cybqa.pesapal.com/pesapalv3/api/Transactions/SubmitOrderRequest"
